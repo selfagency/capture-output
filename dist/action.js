11bf7cc20f8f69a598facd871a52e5ce094bcf56
Daniel Sieradski <daniel@self.agency>
2026-02-21 21:31:31 -0500

    fix: restore build step

---
+++ dist/action.js
@@ -0,0 +1,75 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import { writeFile } from 'node:fs/promises';
+import { performance } from 'node:perf_hooks';
+const errorOut = (data, hideWarning = false) => {
+    if (data?.toLowerCase()?.includes('error') &&
+        !data?.toLowerCase()?.includes('warn') &&
+        !data?.includes('ESLint must be installed') &&
+        !data?.startsWith('error Command failed.')) {
+        core.error(data);
+    }
+    else if (!hideWarning && data?.toLowerCase()?.includes('warn')) {
+        core.warning(data);
+    }
+    else {
+        core.debug(data);
+    }
+};
+(async () => {
+    const op = core.getInput('cmd');
+    const args = core.getInput('args')?.replace(/'/g, '').split(',');
+    const hideWarning = core.getInput('hide-warnings') === 'true';
+    const file = core.getInput('file');
+    const fail = core.getInput('fail') === 'true';
+    let output = '';
+    let stdout = '';
+    let stderr = '';
+    let exitCode = 0;
+    const start = performance.now();
+    try {
+        core.debug('Running command: ' + op + ' ' + args.join(' '));
+        exitCode = await exec.exec(op, args, {
+            listeners: {
+                stdout: (data) => {
+                    stdout += data.toString();
+                    output += data.toString();
+                    core.debug(data.toString());
+                },
+                stderr: (data) => {
+                    stderr += data.toString();
+                    output += data.toString();
+                    errorOut(data.toString(), hideWarning);
+                }
+            }
+        });
+    }
+    catch (err) {
+        core.setFailed(`Unexpected error: ${err.message}`);
+    }
+    core.setOutput('exit-code', exitCode);
+    const end = performance.now();
+    core.setOutput('duration', ((end - start) / 1000).toFixed(2));
+    output = output.trim();
+    core.debug(`\n*************\nFinal output:\n*************\n${output}`);
+    core.setOutput('output', output);
+    core.setOutput('stdout', stdout);
+    core.setOutput('stderr', stderr);
+    try {
+        if (file) {
+            const path = file.split('/');
+            path.pop();
+            const dir = path.join('/');
+            await io.mkdirP(dir);
+            await writeFile(file, output);
+        }
+    }
+    catch (err) {
+        core.setFailed(`Failed to write output to file: ${err.message}`);
+    }
+    if (fail && exitCode != 0) {
+        core.setFailed(stderr);
+    }
+})();
+//# sourceMappingURL=action.js.map
